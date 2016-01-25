d971b4d2bb1b131fa67b2740b36e4e03a4fbce48
Kyle L. Jensen <kljensen+cloud9@gmail.com>
2016-01-25 21:27:37 +0000

    Parse submitted forms

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var app = express();
+var bodyParser = require('body-parser');
 
+// Configure our app
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', function (req, res) {
     res.render('index');
+});
+
+app.post('/user/register', function (req, res) {
+  res.send(req.body);
+  console.log('The user has the email address', req.body.email);
 });
 
 app.listen(process.env.PORT, function () {
