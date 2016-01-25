a894635f15944dfb2b54d88f107118869dae181d
Kyle L. Jensen <kljensen+cloud9@gmail.com>
2016-01-25 19:12:42 +0000

    Add basic handlebars templates

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
 var app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', function (req, res) {
-  res.send('Hello World -- can you believe this!?!?!');
+    res.render('index');
 });
 
 app.listen(process.env.PORT, function () {
