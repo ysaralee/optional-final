29c1c0d414666bc5f264a531973d82592d4869b2
Kyle L. Jensen <kljensen+cloud9@gmail.com>
2016-01-27 18:36:24 +0000

    Add basic session handling in memory

--- index.js
+++ index.js
@@ -2,19 +2,46 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var app = express();
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var Users = require('./models/users.js');
 
 // Configure our app
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: 'auto' }
+}))
+
+
+app.use(function(req, res, next){
+  console.log('req.session =', req.session);
+  if(req.session.userId){
+    Users.findById(req.session.userId, function(err, user){
+      if(!err){
+        res.locals.currentUser = user;
+      }
+      next();
+    })
+  }else{
+    next();
+  }
+})
+
+
 
 app.get('/', function (req, res) {
   Users.count(function (err, users) {
     if (err) {
       res.send('error getting users');
     }else{
-      res.render('index', {userCount: users.length});
+      res.render('index', {
+        userCount: users.length,
+        currentUser: res.locals.currentUser
+      });
     }
   });
 });
@@ -29,7 +56,8 @@ app.post('/user/register', function (req, res) {
   newUser.hashed_password = req.body.password;
   newUser.email = req.body.email;
   newUser.name = req.body.fl_name;
-  newUser.save(function(err){
+  newUser.save(function(err, user){
+    req.session.userId = user._id;
     if(err){
       res.render('index', {errors: err});
     }else{
@@ -38,6 +66,12 @@ app.post('/user/register', function (req, res) {
   })
   console.log('The user has the email address', req.body.email);
 });
+
+
+app.get('/user/logout', function(req, res){
+  req.session.destroy();
+  res.redirect('/');
+})
 
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port ' + process.env.PORT);
