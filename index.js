e011cbe714e2b5987b49d680dfb62ede15ba26b1
Kyle L. Jensen <kljensen+cloud9@gmail.com>
2016-01-27 19:12:08 +0000

    Add session persistance to mongo and user password hashing with bcrypt

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ var exphbs  = require('express-handlebars');
 var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
 var Users = require('./models/users.js');
 
 // Configure our app
+var store = new MongoDBStore({ 
+  uri: process.env.MONGO_URL,
+  collection: 'sessions'
+});
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -13,7 +18,8 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: 'auto' }
+  cookie: { secure: 'auto' },
+  store: store
 }))
 
 
@@ -57,16 +63,38 @@ app.post('/user/register', function (req, res) {
   newUser.email = req.body.email;
   newUser.name = req.body.fl_name;
   newUser.save(function(err, user){
-    req.session.userId = user._id;
     if(err){
+      err = 'Error registering you!';
       res.render('index', {errors: err});
     }else{
+      req.session.userId = user._id;
       res.redirect('/');
     }
   })
   console.log('The user has the email address', req.body.email);
 });
 
+
+app.post('/user/login', function (req, res) {
+  var user = Users.findOne({email: req.body.email}, function(err, user){
+    if(err || !user){
+      res.send('bad login, no such user');
+      return;
+    }
+    console.log('user =', user);
+    console.log('actual password =', user.hashed_password);
+    console.log('provided password =', req.body.password);
+    
+    user.comparePassword(req.body.password, function(err, isMatch){
+      if(err || !isMatch){
+        res.send('bad password duder');
+      }else{
+        req.session.userId = user._id;
+        res.redirect('/')
+      }
+    });
+  })
+});
 
 app.get('/user/logout', function(req, res){
   req.session.destroy();
--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/social-todo');
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
+mongoose.connect(process.env.MONGO_URL);
 
 var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
@@ -15,11 +17,40 @@ var UserSchema = new Schema({
         type: String,
         minlength: 1,
         maxlength: 50,
-        lowercase: true
+        lowercase: true,
+        unique: true
     },
     name: stringField,
     hashed_password: stringField
 });
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('hashed_password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.hashed_password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.hashed_password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.hashed_password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 UserSchema.statics.count = function (cb) {
   return this.model('Users').find({}, cb);
