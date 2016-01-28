d5ae48f998c13a83c2a52575114875b5ff6e6a1b
Kyle L. Jensen <kljensen@gmail.com>
2016-01-28 18:25:45 -0500

    Clean up, comment, and add classes for making end-to-end tests pass

--- index.js
+++ index.js
@@ -12,38 +12,41 @@ var Tasks = require('./models/tasks.js');
 
 
 // Configure our app
-var store = new MongoDBStore({ 
+var store = new MongoDBStore({
   uri: process.env.MONGO_URL,
   collection: 'sessions'
 });
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// Configure session middleware that will parse the cookies
+// of an incoming request to see if there is a session for this cookie.
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: { secure: 'auto' },
   store: store
-}))
+}));
 
-
+// Middleware that looks up the current user for this sesssion, if there
+// is one.
 app.use(function(req, res, next){
-  console.log('req.session =', req.session);
   if(req.session.userId){
     Users.findById(req.session.userId, function(err, user){
       if(!err){
         res.locals.currentUser = user;
       }
       next();
-    })
+    });
   }else{
     next();
   }
-})
+});
 
+// Middleware that checks if a user is logged in. If so, the
+// request continues to be processed, otherwise a 403 is returned.
 function isLoggedIn(req, res, next){
-  console.log('res.locals.currentUser = ', res.locals.currentUser);
   if(res.locals.currentUser){
     next();
   }else{
@@ -51,6 +54,7 @@ function isLoggedIn(req, res, next){
   }
 }
 
+// Middleware that loads a users tasks if they are logged in.
 function loadUserTasks(req, res, next) {
   if(!res.locals.currentUser){
     return next();
@@ -63,67 +67,81 @@ function loadUserTasks(req, res, next) {
         res.locals.tasks = tasks;
       }
       next();
-  })
+  });
 }
 
-
+// Return the home page after loading tasks for users, or not.
 app.get('/', loadUserTasks, function (req, res) {
       res.render('index');
 });
 
+// Handle submitted form for new users
 app.post('/user/register', function (req, res) {
   if(req.body.password !== req.body.password_confirmation){
       return res.render('index', {errors: "Password and password confirmation do not match"});
   }
-  
-  
+  if (req.body.password.length < 1) {
+    err = 'Bad password';
+    res.render('index', {errors: err});
+    return;
+  }
+
+  // Save the new user
   var newUser = new Users();
   newUser.hashed_password = req.body.password;
   newUser.email = req.body.email;
   newUser.name = req.body.fl_name;
   newUser.save(function(err, user){
-    if(err){
-      err = 'Error registering you!';
-      res.render('index', {errors: err});
-    }else{
+    // If there are no errors, redirect to home page
+    if(user && !err){
       req.session.userId = user._id;
       res.redirect('/');
     }
-  })
-  console.log('The user has the email address', req.body.email);
+    var errors = "Error registering you.";
+    if(err){
+      if(err.errmsg && err.errmsg.match(/duplicate/)){
+        errors = 'Account with this email already exists!';
+      }
+      return res.render('index', {errors: errors});
+    }
+  });
 });
+
 
 
 app.post('/user/login', function (req, res) {
-  var user = Users.findOne({email: req.body.email}, function(err, user){
+  // Try to find this user by email
+  Users.findOne({email: req.body.email}, function(err, user){
+
     if(err || !user){
-      res.send('bad login, no such user');
+      res.send('Invalid email address');
       return;
     }
-    console.log('user =', user);
-    console.log('actual password =', user.hashed_password);
-    console.log('provided password =', req.body.password);
-    
+
+    // See if the hash of their passwords match
     user.comparePassword(req.body.password, function(err, isMatch){
       if(err || !isMatch){
-        res.send('bad password duder');
+        res.send('Invalid password');
       }else{
         req.session.userId = user._id;
-        res.redirect('/')
+        res.redirect('/');
       }
     });
-  })
+  });
 });
 
+// Log a user out
 app.get('/user/logout', function(req, res){
   req.session.destroy();
   res.redirect('/');
-})
+});
+
 //  All the controllers and routes below this require
 //  the user to be logged in.
 app.use(isLoggedIn);
 
-app.post('/tasks/create', function(req, res){
+// Handle submission of new task form
+app.post('/task/create', function(req, res){
   var newTask = new Tasks();
   newTask.owner = res.locals.currentUser._id;
   newTask.title = req.body.title;
@@ -136,9 +154,9 @@ app.post('/tasks/create', function(req, res){
       res.redirect('/');
     }
   });
-})
+});
 
-
+// Start the server
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port ' + process.env.PORT);
 });
