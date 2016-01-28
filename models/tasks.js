61edfb49188c045118499f2357be2d955831b000
Kyle L. Jensen <kljensen+cloud9@gmail.com>
2016-01-28 21:30:32 +0000

    Add models/tasks.js

---
+++ models/tasks.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var stringField = {
+    type: String,
+    minlength: 1,
+    maxlength: 500
+}
+
+var TaskSchema = new Schema({
+    owner: ObjectId,
+    title: stringField,
+    description: stringField,
+    isComplete: Boolean,
+    collaborators: [String]
+});
+
+module.exports = mongoose.model('Tasks', TaskSchema);
